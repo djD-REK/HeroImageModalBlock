b745cb0c3424335575350df4457af76bcbb2eba5
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-24 20:04:41 -0400

    Add tests

--- __test__/Block.spec.js
+++ __test__/Block.spec.js
@@ -7,7 +7,7 @@ import {
 import { block as Block, defaultConfig } from "../src"
 
 let props
-describe.skip("The Block", () => {
+describe("The Block", () => {
   beforeEach(() => {
     props = {
       data: {},
@@ -22,14 +22,22 @@ describe.skip("The Block", () => {
   describe("when there is no custom data", () => {
     it("should render the block with the default content", () => {
       const wrapper = mount(<Block {...props} />)
-      expect(wrapper.text()).toBe(defaultConfig.text)
+      const { lineOneText, lineTwoText, buttonText } = defaultConfig
+      expect(wrapper.text()).toBe(lineOneText + lineTwoText + buttonText)
     })
   })
   describe("when given custom data", () => {
     it("should render the block using the custom data", () => {
       const customText = "Custom Block Text"
-      const wrapper = mount(<Block {...props} text={customText} />)
-      expect(wrapper.text()).toBe(customText)
+      const wrapper = mount(
+        <Block
+          {...props}
+          lineOneText={`${customText}1`}
+          lineTwoText={`${customText}2`}
+          buttonText={`${customText}3`}
+        />
+      )
+      expect(wrapper.text()).toBe(`${customText}1${customText}2${customText}3`)
     })
   })
 })
